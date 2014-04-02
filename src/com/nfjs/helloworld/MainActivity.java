e51132b92327a762d1a80bf722d56d2b82d56520
Kenneth Kousen <ken.kousen@kousenit.com>
2014-04-02 18:22:16 -0400

    updated with menu item and refactoring

--- src/com/nfjs/helloworld/MainActivity.java
+++ src/com/nfjs/helloworld/MainActivity.java
@@ -3,12 +3,15 @@ package com.nfjs.helloworld;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
     private Button b;
     private EditText et;
 
@@ -38,8 +41,19 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    		switch (item.getItemId()) {
+			case R.id.action_settings:
+				Log.d(TAG, "menu settings selected");
+				return true;
+
+			default:
+				return super.onOptionsItemSelected(item);
+			}
     }
 }
